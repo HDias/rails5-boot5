e54de61ae93928bcdfd9cb978908cfffa81091f0
Horecio Araújo Dias <horecio@gmail.com>
2021-02-21 21:46:42 -0300

    Add devise and pudint

---
+++ app/policies/article_policy.rb
@@ -0,0 +1,5 @@
+class ArticlePolicy < ApplicationPolicy
+  # def index?
+  #   user.admin? or not record.published?
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+  
+  devise_for :users
   get "/articles", to: "articles#index"
 end
